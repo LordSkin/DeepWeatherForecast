507c12c884e80de068b533f37431c4ecc892664f
LordSkin <bartek.kramnik@wp.pl>
2020-05-12 20:00:32 +0200

    update modelu

--- notebook/script/main/loss_functions.py
+++ notebook/script/main/loss_functions.py
@@ -1,4 +1,3 @@
-
 def mean_square_loss_func(y_actual, y_predicted):
     return ((y_actual[0, 0] - y_predicted[0, 0]) ** 2 +
             (y_actual[0, 1] - y_predicted[0, 1]) ** 2 +
@@ -19,3 +18,25 @@ def mean_square_loss_func(y_actual, y_predicted):
             (y_actual[0, 16] - y_predicted[0, 16]) ** 2 +
             (y_actual[0, 17] - y_predicted[0, 17]) ** 2 +
             (y_actual[0, 18] - y_predicted[0, 18]) ** 2) / 18
+
+
+def mean_proportional_loss_func(y_actual, y_predicted):
+    return (abs(y_actual[0, 0] - y_predicted[0, 0]) +
+            abs(y_actual[0, 1] - y_predicted[0, 1]) +
+            abs(y_actual[0, 2] - y_predicted[0, 2]) +
+            abs(y_actual[0, 3] - y_predicted[0, 3]) +
+            abs(y_actual[0, 4] - y_predicted[0, 4]) +
+            abs(y_actual[0, 5] - y_predicted[0, 5]) +
+            abs(y_actual[0, 6] - y_predicted[0, 6]) +
+            abs(y_actual[0, 7] - y_predicted[0, 7]) +
+            abs(y_actual[0, 8] - y_predicted[0, 8]) +
+            abs(y_actual[0, 9] - y_predicted[0, 9]) +
+            abs(y_actual[0, 10] - y_predicted[0, 10]) +
+            abs(y_actual[0, 11] - y_predicted[0, 11]) +
+            abs(y_actual[0, 12] - y_predicted[0, 12]) +
+            abs(y_actual[0, 13] - y_predicted[0, 13]) +
+            abs(y_actual[0, 14] - y_predicted[0, 14]) +
+            abs(y_actual[0, 15] - y_predicted[0, 15]) +
+            abs(y_actual[0, 16] - y_predicted[0, 16]) +
+            abs(y_actual[0, 17] - y_predicted[0, 17]) +
+            abs(y_actual[0, 18] - y_predicted[0, 18])) / 18
